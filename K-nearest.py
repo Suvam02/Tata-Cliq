e00b282b87462bc1ace3f7ed1f96cc28310287dc
Soumyadip Bhattacharjee <63645852+Suvam02@users.noreply.github.com>
2023-03-08 23:47:02 +0530

    Update K-nearest.py

--- K-nearest.py
+++ K-nearest.py
@@ -4,8 +4,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 
-# Load the dataset
-data = pd.read_csv('train.csv')
+
+# Load the dataset from the train dataset > Link in PPT Slide 4
+data = pd.read_csv('train.csv') 
 X = data.iloc[:,2].values
 y = data.iloc[:, -2].values
 
@@ -17,16 +18,16 @@ print(y_train)
 print(X_test)
 print(y_test)
 # Split the data into training and testing sets
-## X_train, X_test, y_train, y_test = train_test_split(data['City'], data['Gender'], test_size=0.2, random_state=42)
 
-# Convert the city names to feature vectors using CountVectorizer
+
+# Convert the p_brand names to feature vectors using CountVectorizer
 vectorizer = CountVectorizer()
 X_train_vec = vectorizer.fit_transform(X_train)
 X_test_vec = vectorizer.transform(X_test)
 
-# Train a Naive Bayes classifier on the training data
+# Train a KNN classifier on the training data
 from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier(n_neighbors = 2, metric = 'minkowski', p = 2)
+classifier = KNeighborsClassifier(n_neighbors = 1, metric = 'minkowski', p = 3)
 classifier.fit(X_train_vec, y_train)
 
 
@@ -34,18 +35,14 @@ classifier.fit(X_train_vec, y_train)
 accuracy = classifier.score(X_test_vec, y_test)
 print(f'Accuracy: {accuracy}')
 
-# Use the classifier to predict the gender of people living in new cities
-
+# Use the classifier to predict the gender of people 
+# Load the dataset from the test dataset > Link in PPT Slide 4
 new_test=pd.read_csv('test.csv')['p_brand']
 new_test_vec = vectorizer.transform(new_test)
 predictions = classifier.predict(new_test_vec)
 print(f'Predictions: {predictions}')
 
-from sklearn.metrics import confusion_matrix, accuracy_score
-cm = confusion_matrix(y_test, classifier.predict(X_test_vec))
-print(cm)
-
-t=pd.read_csv('test copy.csv')
-t['Gender']=predictions
-t.to_csv('test copy.csv')
+t=pd.read_csv('Submission_Team Hacksmiths.csv')
+t['customer_gender_pred']=predictions
+t.to_csv('Submission_Team Hacksmiths.csv')
 
